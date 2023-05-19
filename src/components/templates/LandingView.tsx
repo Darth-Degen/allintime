0f7d360d6baaa6782690d0488d8a91b0169ccea0
mattdegods <matthewcpfeifer@gmail.com>
2023-05-19 11:57:54 -0400

    Fix: ensure teal gutters always visible

--- src/components/templates/LandingView.tsx
+++ src/components/templates/LandingView.tsx
@@ -13,49 +13,53 @@ const LandingView: FC<Props> = (props: Props) => {
   const { showView } = useContext(ViewContext);
 
   return (
-    <div
-      className="bg-custom-black max-w-[1600px] mx-auto w-full
-      h-full flex flex-col items-center justify-center"
-    >
-      <motion.div {...enterAnimation} className="w-full px-10">
-        <LandingBanner />
-        <div className="relative w-full">
-          {/* header text */}
-          <div
-            className="absolute left-1/2 -translate-x-1/2 top-1/2 -translate-y-1/2  
+    <div className="w-full h-full bg-custom-teal">
+      <div
+        className="w-[90%] mx-auto h-full bg-custom-black hidden lg:flex flex-col 
+      items-center justify-center px-20"
+      >
+        <motion.div {...enterAnimation} className="w-full px-10">
+          <LandingBanner />
+          <div className="relative w-full">
+            {/* header text */}
+            <div
+              className="absolute left-1/2 -translate-x-1/2 top-1/2 -translate-y-1/2  
             text-center opacity-80 whitespace-nowrap font-mk-hell text-custom-teal"
-          >
-            <p className="text-[140px] xl:text-[180px] pt-20">all in time</p>
-            <p className="text-xl mt-20">
-              a virtuous lifestyle,
-              <br />
-              with very little patience
-            </p>
-          </div>
-          {/* footer text */}
-          <div
-            className="absolute left-1/2 -translate-x-1/2 bottom-14 w-full  
+            >
+              <p className="text-[140px] xl:text-[180px] pt-20">all in time</p>
+              <p className="text-xl mt-20">
+                a virtuous lifestyle,
+                <br />
+                with very little patience
+              </p>
+            </div>
+            {/* footer text */}
+            <div
+              className="absolute left-1/2 -translate-x-1/2 bottom-14 w-full  
             text-center opacity-80 whitespace-nowrap px-8"
-          >
-            <div className="w-full flex justify-center items-center">
-              <p className="font-secondary text-custom-teal mr-5">VOLUME 1</p>
-              <div className="w-full h-[1px] bg-transparent border-b border-dotted border-custom-teal" />
-              <p className="font-secondary text-custom-teal ml-5">ESSENTIALS</p>
+            >
+              <div className="w-full flex justify-center items-center">
+                <p className="font-secondary text-custom-teal mr-5">VOLUME 1</p>
+                <div className="w-full h-[1px] bg-transparent border-b border-dotted border-custom-teal" />
+                <p className="font-secondary text-custom-teal ml-5">
+                  ESSENTIALS
+                </p>
+              </div>
+            </div>
+            <div className="w-fit mx-auto pl-14">
+              <Image
+                src="/images/landing/intro.png"
+                alt="RULEBREAKERS"
+                width={1494 * 0.4}
+                height={2060 * 0.4}
+                onLoadingComplete={() =>
+                  setAssets && setAssets((prevState) => [(prevState[0] = true)])
+                }
+              />
             </div>
           </div>
-          <div className="w-fit mx-auto pl-14">
-            <Image
-              src="/images/landing/intro.png"
-              alt="RULEBREAKERS"
-              width={1494 * 0.4}
-              height={2060 * 0.4}
-              onLoadingComplete={() =>
-                setAssets && setAssets((prevState) => [(prevState[0] = true)])
-              }
-            />
-          </div>
-        </div>
-      </motion.div>
+        </motion.div>
+      </div>
     </div>
   );
 };
