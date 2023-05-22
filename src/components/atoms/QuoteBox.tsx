16daf4778a35152c9e6d641eda50c9c165f07627
mattdegods <matthewcpfeifer@gmail.com>
2023-05-22 18:41:28 -0400

    Fix: ultrawide responsiveness

--- src/components/atoms/QuoteBox.tsx
+++ src/components/atoms/QuoteBox.tsx
@@ -10,28 +10,31 @@ interface Props {
 
 const QuoteBox: FC<Props> = ({ quote, author, icon }) => {
   const [width] = useWindowSize();
-  const tabletView = width < 1536 && width > 1024;
+  const ultraWideView = width > 1920;
+  const tabletView = width < 1536;
   const mobileView = width < 1024;
 
   return (
     <div
-      className="quote w-[150%] min-h-[85px] 2xl:min-h-[115px] bg-white 
+      className="3xl:mt-20 quote w-[150%] min-h-[85px] 2xl:min-h-[115px] bg-white 
       flex justify-center items-center border-2 border-black rounded-lg drop-shadow-card"
     >
-      <div className="w-full flex items-center justify-evenly">
+      <div className="w-full flex items-center justify-evenly py-1 3xl:py-4">
         {!mobileView && (
           <Image
             src={icon}
-            width={tabletView ? 80 : 100}
-            height={tabletView ? 80 : 100}
+            width={ultraWideView ? 150 : tabletView ? 70 : 100}
+            height={ultraWideView ? 150 : tabletView ? 70 : 100}
             alt="reviewer icon"
             className="px-2"
           />
         )}
 
         <div className="w-full xl:w-2/3 h-full flex flex-col items-start justify-evenly py-2 xl:py-6 px-1">
-          <p className="text-[9px] xl:text-sm">{author}</p>
-          <p className="font-mk-hell text-[10px] xl:text-sm 2xl:text-base">
+          <p className="text-[9px] xl:text-sm 4xl:text-base font-mk-hell">
+            {author}
+          </p>
+          <p className="font-mk-hell text-[10px] xl:text-sm 2xl:text-base 4xl:text-lg">
             {quote}
           </p>
         </div>
--- src/components/organisms/MerchQuotes.tsx
+++ src/components/organisms/MerchQuotes.tsx
@@ -15,13 +15,13 @@ const MerchQuotes = () => {
     >
       <VirtuousLifestyleIcon
         fill="#939393"
-        className="absolute bottom-14 left-10 w-52 2xl:w-80 h-fit"
+        className="absolute bottom-14 left-10 2xl:left-16 w-52 2xl:w-80 3xl:w-96 h-fit"
       />
-      <GlobeIcon className="absolute bottom-14 right-10 w-16 2xl:w-28 h-fit" />
+      <GlobeIcon className="absolute bottom-14 right-10 2xl:right-16 w-16 2xl:w-28 3xl:w-36 h-fit" />
       <div className="absolute top-5 right-8 h-6 2xl:h-8 w-fit">
         <div className="flex items-center gap-5">
-          <NoteIcon1 className="h-6 2xl:h-8 w-fit" />
-          <NoteIcon2 className="h-6 2xl:h-8 w-fit" />
+          <NoteIcon1 className="h-6 2xl:h-8 3xl:h-10 w-fit" />
+          <NoteIcon2 className="h-6 2xl:h-8 3xl:h-10 w-fit" />
         </div>
       </div>
       <QuoteTabs className="absolute bottom-6 left-1/2 -translate-x-1/2 h-fit w-[90%] 2xl:w-[95%]" />
